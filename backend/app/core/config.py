22c2a9cdfde9f94b124836c8935ba7e1ded0cb22
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-08-17 18:54:36 +0300

    make the project structure correctlly and update the config file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,4 +3,5 @@ class Settings(BaseSettings):
     DATABASE_URL : str 
     model_config = SettingsConfigDict(env_file=".env")
 
+# in this step we make a instance from settings so every file in the project can make a import 
 settings = Settings()
