8ebf333acb2c117378c0af1baf0e4ee7bbc36d30
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-08-20 02:32:52 +0300

    edit in some extra line and add some comments to the code

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,10 +1,10 @@
 from pydantic_settings import BaseSettings , SettingsConfigDict
 class Settings(BaseSettings):
-    DATABASE_URL : str 
+    DATABASE_URL:str 
     JWT_SECRET_KEY:str
     JWT_ALGORITHM:str
     ACCESS_TOKEN_EXPIRE_MINUTES:int
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config=SettingsConfigDict(env_file=".env")
 
 # in this step we make a instance from settings so every file in the project can make a import 
 settings = Settings()
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -5,9 +5,11 @@ from pwdlib import PasswordHash
 
 password_hasher = PasswordHash.recommended()
 
+# hashing password
 def hash_password(password: str) -> str:
     return password_hasher.hash(password)
 
+# vertify if the user enter the password correctly or not 
 def verify_password(password: str, hashed_password: str) -> bool:
     return password_hasher.verify(password, hashed_password)
 
