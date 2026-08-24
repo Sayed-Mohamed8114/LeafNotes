d453285f5d3c24ca10ce7d5b62240030495275d7
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-08-24 18:08:58 +0300

    make auth context to share the token between elements

---
+++ frontend/src/Context/AurhContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext(null);
+
+export function AuthProvider({ children }) {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  const login = (accessToken) => {
+    localStorage.setItem("token", accessToken);
+
+    setToken(accessToken);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+
+    setToken(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
