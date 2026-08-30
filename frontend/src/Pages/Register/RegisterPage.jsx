0955cfd1e97ea2639c15c6ce47f9a60707d8b4c7
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-08-30 16:56:40 +0300

    fix some thing in the responsive design

--- frontend/src/Pages/Register/RegisterPage.jsx
+++ frontend/src/Pages/Register/RegisterPage.jsx
@@ -20,7 +20,7 @@ export default function RegisterPage() {
       )}
       <section className=" w-full p-5 md:p-10 flex items-center justify-center bg-gray-950 text-teal-50 gap-10 min-h-screen ">
         <div className=" w-[99%] lg:w-[50%] bg-gray-900/80 p-5 md:p-10 rounded-2xl shadow-teal-900 shadow-xl flex items-center flex-col-reverse md:flex-row justify-center gap-10 h-auto md:h-[70%] ">
-          <div className=" h-auto w-full md:w-[50%] items-center justify-center flex gap-10 flex-col ">
+          <div className=" h-auto w-full md:w-[60%] items-center justify-center flex gap-10 flex-col ">
             <h2 className=" font-['Black_Ops_One'] text-center md:text-start text-2xl md:text-5xl ">
               Join leaf-Note
             </h2>
@@ -28,16 +28,16 @@ export default function RegisterPage() {
               Create your leaf-Note account and start managing your daily tasks
               and todos with a simple and beautiful experience.
             </p>
-            <div className=" mt-2 md:mt-10 flex w-full items-center justify-evenly ">
+            <div className=" mt-2 md:mt-10 gap-2 flex w-full items-center justify-evenly ">
               <button
                 onClick={() => handleNavigation("/")}
-                className=" text-sm md:text-lg text-teal-300 underline underline-offset-3 hover:text-teal-50 transition duration-700 "
+                className=" text-sm md:text-sm lg:text-lg text-teal-300 underline underline-offset-3 hover:text-teal-50 transition duration-700 "
               >
                 Back to home?
               </button>
               <button
                 onClick={() => handleNavigation("/login")}
-                className=" text-sm md:text-lg text-teal-100 underline underline-offset-3 hover:text-gray-200 transition duration-700 "
+                className=" text-sm md:text-sm lg:text-lg text-teal-100 underline underline-offset-3 hover:text-gray-200 transition duration-700 "
               >
                 Already have an account?
               </button>
